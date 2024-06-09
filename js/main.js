7f8243549a621841d39a4d018e1fce22bac9ca9e
Guilherme-Gomes-Luccas <guilhermegomes.luccas@gmail.com>
2024-06-09 00:16:58 -0300

    adição da area de comentario e das funções de adição de comentario e remoção do card

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ const searchGame = async (event) => {
     event.preventDefault();
 
     const search = document.getElementById('nome').value;
-    console.log('Nome do jogo:', search);
+    //console.log('Nome do jogo:', search);
 
     document.getElementById('busca_text').innerText = `Resultado da busca para: ${search}`;
 
@@ -35,7 +35,7 @@ const searchGame = async (event) => {
 
         const result = await response.json();
 
-        console.log(result);
+        //console.log(result);
 
         let searchGame = "";
 
@@ -46,8 +46,8 @@ const searchGame = async (event) => {
                 <img src=${element.logo} alt="logo">
                 <a>${element.name}</a>
                 <div class="gametype">
-                    <button onclick="libraryAppid(${element.appid})">Library</button>
-                    <button onclick="wishAppid(${element.appid})">Wish List</button>
+                    <button onclick="libraryAppid(${element.appid})" class="gameTypeButton">Library</button>
+                    <button onclick="wishAppid(${element.appid})" class="gameTypeButton">Wish List</button>
                 </div>
             </div>`;
         });
@@ -65,8 +65,8 @@ function libraryAppid(appid) {
     const novoItem = { appid: appid, comentario: "" };
 
     library.push(novoItem);
-    library = removeDuplicates(library)
-    console.log(library);
+    library = removeDuplicates(library);
+    //console.log(library);
     saveToLocalStorage(); // Salva o array atualizado no localStorage
 }
 
@@ -75,16 +75,16 @@ function wishAppid(appid) {
     const novoItem = { appid: appid, comentario: "" };
 
     wish.push(novoItem);
-    wish = removeDuplicates(wish)
-    console.log(wish);
+    wish = removeDuplicates(wish);
+    //console.log(wish);
     saveToLocalStorage(); // Salva o array atualizado no localStorage
 }
 
 // Função para carregar cartões da biblioteca
 const libraryCards = async (event) => {
-    event.preventDefault();
+    if (event) event.preventDefault();
 
-    console.log(library);
+    //console.log(library);
 
     for (const appid of library) {
         const game = await loadCards(appid.appid);
@@ -96,7 +96,7 @@ const libraryCards = async (event) => {
                 <img src="${gameData.header_image}" alt="logo">
                 <a>${gameData.name}</a>
                 <div class="gametype">
-                    <button onclick="detailsModal(event)">Ver detalhes</button>
+                    <button onclick="detailsModal(event, 'library')" class="gameTypeButton">Ver detalhes</button>
                 </div>
             </div>`;
         }
@@ -105,22 +105,22 @@ const libraryCards = async (event) => {
 
 // Função para carregar cartões da lista de desejos
 const wishCards = async (event) => {
-    event.preventDefault();
+    if (event) event.preventDefault();
 
-    console.log(wish);
+    //console.log(wish);
 
     for (const appid of wish) {
         const game = await loadCards(appid.appid);
         if (game && game[appid.appid] && game[appid.appid].success) {
             const gameData = game[appid.appid].data;
-            console.log(gameData + "oi");
+            //console.log(gameData + "oi");
             document.getElementById('mainwish').innerHTML += `
             <div class="game">
                 <a hidden class="appid">${appid.appid}</a>
                 <img src="${gameData.header_image}" alt="logo">
                 <a>${gameData.name}</a>
                 <div class="gametype">
-                    <button onclick="detailsModal(event)">Ver detalhes</button>
+                    <button onclick="detailsModal(event, 'wish')" class="gameTypeButton">Ver detalhes</button>
                 </div>
             </div>`;
         }
@@ -138,7 +138,7 @@ async function loadCards(appid) {
 
         const result = await response.json();
 
-        console.log(result);
+        //console.log(result);
 
         return result;
     } catch (error) {
@@ -147,52 +147,104 @@ async function loadCards(appid) {
     }
 }
 
-const detailsModal = async (event) => {
+const detailsModal = async (event, pageType) => {
     try {
-        const appid = event.target.closest('.game').querySelector('.appid').textContent
-        console.log(appid + "bunda")
-        modal = event.target.closest('.main').querySelector('.modal')
-        
-        const response = await fetch(`https://cors-anywhere.herokuapp.com/https://store.steampowered.com/api/appdetails?appids=${appid}`);
-        
+        const appid = event.target.closest('.game').querySelector('.appid').textContent;
+        const modal = event.target.closest('.main').querySelector('.modal');
+
+        if (!modal) {
+            console.error("Modal element not found.");
+            return;
+        }
+
+        const response = await fetch(`https://cors-anywhere.herokuapp.com/https://store.steampowered.com/api/appdetails?cc=BR&appids=${appid}`);
         if (!response.ok) {
             throw new Error('Erro ao consultar ação!');
         }
 
         const result = await response.json();
+        const gameData = result[appid].data;
 
-        console.log(result);
-        
+        console.log(result)
 
-        document.querySelector('.modal-content').innerHTML = `
-            <span class="close" onclick="closeModal()">&times;</span>
-            <h2>${result[appid].data.name}</h2>
-            <img src="${result[appid].data.header_image}" alt="logo">
-            <div class="description">${result[appid].data.short_description}</div>
-            <div class="requirements">${result[appid].data.pc_requirements.minimum}</div>
-            <div class="price">
-                <a><strong>Preço: </strong>${result[appid].data.price_overview.final_formatted}</a>
-            </div>
-        `
-
-        if (event.target.closest('.main').id === "mainlibrary") {
-            console.log(event.target.closest('.main').id)
-            document.querySelector('.modal-content').innerHTML += `
-                <div class="comment">
-                    <textarea></textarea>
-                </div>
-            `
-        } else {
-            console.log(event.target.closest('.main').id)
+        if (!gameData) {
+            console.error("Game data not found.");
+            return;
         }
 
-        modal.style.display = 'block'
+        const modalContent = document.querySelector('.modal-content');
+        if (!modalContent) {
+            console.error("Modal content element not found.");
+            return;
+        }
+
+        const gameComment = (pageType === "library") ? library.find(item => item.appid == appid).comentario : wish.find(item => item.appid == appid).comentario;
+
+        modalContent.innerHTML = `
+            <span class="close" onclick="closeModal()">&times;</span>
+            <div class="title">
+                <h2>${gameData.name}</h2>
+                <img src="${gameData.header_image}" alt="logo">
+            </div>
+            <br>
+            <div class="description">${gameData.short_description}</div>
+            <div class="requirements">${gameData.pc_requirements.minimum}</div>
+            <div class="price">
+                <a><strong>Preço: </strong>${gameData.price_overview.final_formatted ? gameData.price_overview.final_formatted : "N/A"}</a>
+            </div>
+            <br>
+            <div class="comment">
+                <textarea class="textarea" placeholder="Digite seu comentário caso tenha o jogo ou digite sua expectativa para esse jogo">${gameComment}</textarea>
+            </div>
+            <div class="buttons">
+                <div class="gametype">
+                    <button onclick="addComment(${appid}, '${pageType}')" class="buttonGame">Adicionar comentario</button>
+                </div>
+                <div class="gametype">
+                    <button onclick="deleteGame(${appid}, '${pageType}')" class="buttonGame">Remover jogo</button>
+                </div>
+            </div>
+        `;
+
+        modal.style.display = 'block';
     } catch (error) {
         alert('Erro ao consultar ação: ' + error.message);
         console.error('ERROR', error);
     }
 }
 
+
+function addComment(appid, pageType){
+    const comentario = document.querySelector('.modal-content .textarea').value;
+    if (pageType === "library") {
+        const game = library.find(item => item.appid == appid);
+        if (game) {
+            game.comentario = comentario;
+            //console.log(game)
+        }
+    } else if(pageType === "wish"){
+        const game = library.find(item => item.appid == appid);
+        if (game) {
+            game.comentario = comentario;
+            //console.log(game)
+        }
+    }
+    saveToLocalStorage();
+    closeModal();
+}
+
+function deleteGame(appid, pageType) {
+    if (pageType === "library") {
+        library = library.filter(item => item.appid != appid);
+    } else if (pageType === "wish") {
+        wish = wish.filter(item => item.appid != appid);
+    }
+    saveToLocalStorage();
+    closeModal();
+    // Recarrega a página
+    window.location.reload();
+}
+
 function closeModal() {
     document.querySelector('.modal').style.display = "none";
-  }
+}
